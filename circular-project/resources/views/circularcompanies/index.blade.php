3b9b416d722fc9d4da6f36efb9089a91bfb7b86b
Vandevelde <raoul.vandevelde@gmail.com>
2023-06-11 17:50:09 +0200

    Add more details to the companies overview with styling to center the table

--- circular-project/resources/views/circularcompanies/index.blade.php
+++ circular-project/resources/views/circularcompanies/index.blade.php
@@ -7,12 +7,14 @@
     <div class="companies-table-container">
         <div class="table-container">
             <h1 class="text-3xl font-bold mb-5 p-2 pb-5 text-center">Companies</h1>
-            <div class="overflow-x-auto">
+            <div class="table-content">
                 <table class="companies-table">
                     <thead>
                         <tr>
                             <th class="header-table-company">Company</th>
-                            <th class="header-table-table">Type</th>
+                            <th class="header-table-type">Type</th>
+                            <th class="header-table-address">Address</th>
+                            <th class="header-table-country">Country</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -23,6 +25,12 @@
                                     {{ $company->name }}</td>
                                 <td class="table-companies-type">
                                     {{ $company->type }}
+                                </td>
+                                <td class="table-companies-address">
+                                    {{ $company->address }}
+                                </td>
+                                <td class="table-companies-country">
+                                    {{ $company->country }}
                                 </td>
                             </tr>
                         @endforeach
